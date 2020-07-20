1fc26b3756062281997836f227ca782a93b0d407
blackplusy <osimeno@163.com>
2020-07-20 10:41:06 +0800

    Add files via upload

---
+++ 例子-0720-02.python的输入.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+#输入就是把input函数的值传入变量中，操作变量
+#name=input()
+#print('your name is  %s'% name)
+
+name=input('请输入您的法号！')
+print('您的法号是%s'% name)
+print(type(name))
+
+name=int(input('请输入第一个数字'))
+name2=int(input('请输入第二个数字'))
+print(name+name2)
